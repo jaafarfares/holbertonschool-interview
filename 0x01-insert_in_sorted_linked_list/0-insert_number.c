11ab08ecfd1758e9b67c6adee9394bf4eb2167a9
jaafar <jaafarferes@gmail.com>
2022-12-18 20:18:32 +0100

    insert a node in a sorted linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -14,25 +14,28 @@
 */
 
 
+
+
+
+
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t **current;
+	listint_t *new_node, **tmp;
 
-	current = head;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	tmp = head;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	{
 		return (NULL);
+	}
+	new_node->n = number;
 
-	new->n = number;
-
-		while ((*current) != NULL && (*current)->n < number)
-		{
-			current = &(*current)->next;
-		}
-	new->next = (*current);
-	(*current) = new;
-	return (new);
+	while ((*tmp) && (*tmp)->n < number)
+	{
+		tmp = &(*tmp)->next;
+	}
+	new_node->next = (*tmp);
+	(*tmp) = new_node;
+	return (new_node);
 }
-
