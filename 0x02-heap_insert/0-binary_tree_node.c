266f44400d787e16995e22503dc33db1fc5583ea
jaafar <jaafarferes@gmail.com>
2022-12-22 14:16:59 +0100

    0. New node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,14 +1,25 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_node - .....
+ * @parent: ....
+ * @value: ....
+ * Return: ....
+ *
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *tmp;
+
 
     tmp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
     tmp->left = tmp->right = NULL;
     tmp->n = value;
     tmp->parent = parent;
         return (tmp);
+
     return (NULL);
 }
