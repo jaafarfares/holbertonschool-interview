235547315e4ade472264d7766ec5091668c3764f
jaafar <jaafarferes@gmail.com>
2023-01-04 22:10:08 +0100

    function to count minimum operations to duplicate the character H     till it reach the n number

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,25 +9,19 @@ def minOperations(n):
     function to count minimum operations to duplicate the character H
     till it reach the n number
     """
+
+
     counter = 0
     i = 1
-    
-    if (n <= 0):
-        return 0
-    
+    a = 0 
     
     while i < n:
-        if (i % 2 != 0):
+        if n % i == 0:
             a = i
-            counter = counter + 2
-            i = i * 2
-            if i + (a * 2) == n:
-                counter = counter + 2
-                return counter
-        if (i % 2 == 0):
             counter = counter + 1
-            i = i + a
-            a = i
+            i = i * 2
         else:
-            return 0
+            i += a
+        counter += 1
+
     return counter
