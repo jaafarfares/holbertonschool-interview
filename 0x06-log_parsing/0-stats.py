09c67c4677084124388dad96fad4655fbebdd7b1
jaafar <jaafarferes@gmail.com>
2023-01-26 16:43:34 +0100

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,34 +7,31 @@ import datetime
 from collections import Counter
 
 
-if __name__ == "__main__":
-    """main function"""
 counter = 0
 total_f_size = 0
-status_codes = Counter({'200': 0, '301': 0, '400': 0,
-               '401': 0, '403': 0, '404': 0, '405': 0, '500': 0})
+status_codes = {'200': 0, '301': 0, '400': 0,
+                '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 try:
     for line in sys.stdin:
-        try:
-            if line and status_codes:
-                counter += 1
-                f_size = int(line.split()[-1].strip())
-                s_code = line.split()[-2].strip()
-                total_f_size += f_size
-                status_codes[s_code] += 1
-                f_size = 0
-                if counter % 10 == 0:
-                    print("File size: " + str(total_f_size))
-                    for key in sorted(status_codes.keys()):
-                        print(key + ": " + str(status_codes[key]))
-        except KeyboardInterrupt:
-            if counter % 10 == 0:
-                print("File size: " + str(total_f_size))
-                for key in sorted(status_codes.keys()):
+        counter += 1
+        f_size = int(line.split()[-1].strip())
+        s_code = line.split()[-2].strip()
+        total_f_size += f_size
+        if s_code in status_codes.keys():
+            status_codes[s_code] += 1
+        f_size = 0
+        if counter % 10 == 0:
+            print("File size: " + str(total_f_size))
+            for key in sorted(status_codes.keys()):
+                if status_codes[key] != 0:
                     print(key + ": " + str(status_codes[key]))
+    print("File size: " + str(total_f_size))
+    for key in sorted(status_codes.keys()):
+        if status_codes[key] != 0:
+            print(key + ": " + str(status_codes[key]))
 except KeyboardInterrupt:
-    if counter % 10 == 0:
-        print("File size: " + str(total_f_size))
-        for key in sorted(status_codes.keys()):
+    print("File size: " + str(total_f_size))
+    for key in sorted(status_codes.keys()):
+        if status_codes[key] != 0:
             print(key + ": " + str(status_codes[key]))
     raise
