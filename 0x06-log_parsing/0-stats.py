08137dfbe5e3f88d4e1db5c40a79ef872cc324c8
jaafar <jaafarferes@gmail.com>
2023-01-26 11:59:52 +0100

    Log parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that reads stdin line by line and computes metrics
+"""
+import sys
+import datetime
+from collections import Counter
+
+
+form = "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n"
+counter = 0
+total_f_size = 0
+status_codes = Counter()
+
+for line in sys.stdin:
+    try:
+        if line and form:
+            counter += 1
+            f_size = int(line.split()[-1].strip())
+            s_code = line.split()[-2].strip()
+            total_f_size += f_size
+            status_codes[s_code] += 1
+            f_size = 0
+            if counter % 10 == 0:
+                print("File size: " + str(total_f_size))
+                for key, num in sorted(status_codes.items()):
+                    print(str(key) + ": " + str(num))
+    except ValueError as e:
+        print(e)
