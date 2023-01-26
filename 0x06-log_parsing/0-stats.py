229b9147aa07bdead5c3f7fc456a994aa04e5965
jaafar <jaafarferes@gmail.com>
2023-01-26 16:47:05 +0100

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,18 +13,21 @@ status_codes = {'200': 0, '301': 0, '400': 0,
                 '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 try:
     for line in sys.stdin:
-        counter += 1
-        f_size = int(line.split()[-1].strip())
-        s_code = line.split()[-2].strip()
-        total_f_size += f_size
-        if s_code in status_codes.keys():
-            status_codes[s_code] += 1
-        f_size = 0
-        if counter % 10 == 0:
-            print("File size: " + str(total_f_size))
-            for key in sorted(status_codes.keys()):
-                if status_codes[key] != 0:
-                    print(key + ": " + str(status_codes[key]))
+        try:
+            counter += 1
+            f_size = int(line.split()[-1].strip())
+            s_code = line.split()[-2].strip()
+            total_f_size += f_size
+            if s_code in status_codes.keys():
+                status_codes[s_code] += 1
+            f_size = 0
+            if counter % 10 == 0:
+                print("File size: " + str(total_f_size))
+                for key in sorted(status_codes.keys()):
+                    if status_codes[key] != 0:
+                        print(key + ": " + str(status_codes[key]))
+        except Exception:
+            pass
     print("File size: " + str(total_f_size))
     for key in sorted(status_codes.keys()):
         if status_codes[key] != 0:
