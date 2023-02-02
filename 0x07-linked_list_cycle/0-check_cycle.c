d9cee841d7caff1a4e551793debd294c20158792
jaafar <jaafarferes@gmail.com>
2023-02-02 18:52:05 +0100

    first commit

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -12,12 +12,13 @@
 int check_cycle(listint_t *list)
 {
     listint_t *ptr;
-    ptr = list->next;
-    while (ptr != NULL)
+    ptr = list;
+    while (ptr != NULL && ptr->next)
     {
         if (ptr == list)
             return (1);
-        ptr = ptr->next;
+        ptr = ptr->next->next;
+        list = list->next;
     }
     return 0;
 }
