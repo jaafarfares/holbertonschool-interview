5ca00fd31071789f9799b1caad29bcfd64289662
jaafar <jaafarferes@gmail.com>
2023-02-04 11:54:19 +0100

    check if a linked list is a palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -6,22 +6,26 @@
  * Return: int.
  */
 
-int is_palindrome(listint_t **head) {
+int is_palindrome(listint_t **head)
+{
     listint_t *current = *head;
     int len = 0;
     int arr[1024];
 
-    while (current != NULL) {
+    while (current != NULL)
+    {
         arr[len] = current->n;
         current = current->next;
         len++;
     }
 
-    for (int i = 0; i < len / 2; i++) {
-        if (arr[i] != arr[len - 1 - i]) {
-            return 0;
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (arr[i] != arr[len - 1 - i])
+        {
+            return (0);
         }
     }
 
-    return 1;
+    return (1);
 }
