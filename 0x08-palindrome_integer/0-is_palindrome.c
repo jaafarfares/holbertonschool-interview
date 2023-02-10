27179bf7539207776c1bcf3f29af5baead99aad6
jaafar <jaafarferes@gmail.com>
2023-02-10 11:33:59 +0100

    first commit

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,24 +1,24 @@
 #include "palindrome.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * is_palindrome - .......
  * @n: ......
  * Return: .......
- * 
- * 
 */
 
 int is_palindrome(unsigned long n)
 {
-    unsigned long r,sum=0,temp;    
-    temp=n;    
-    while(n>0)    
-    {    
-    r=n%10;    
-    sum=(sum*10)+r;    
-    n=n/10;    
-    }    
-    if(temp==sum)    
-    return (1);
-    else    
-    return 0;  
+unsigned long r, sum = 0, temp;
+temp = n;
+while (n > 0)
+{
+r = n % 10;
+sum = (sum * 10) + r;
+n = n / 10;
+}
+if (temp == sum)
+return (1);
+else
+return (0);
 }
