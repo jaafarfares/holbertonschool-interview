7eda01a3f38132d77d43917b9b0bb484e0c9f201
jaafar <jaafarferes@gmail.com>
2023-02-10 11:28:49 +0100

    first commit

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "palindrome.h"
+/**
+ * is_palindrome - .......
+ * @n: ......
+ * Return: .......
+ * 
+ * 
+*/
+
+int is_palindrome(unsigned long n)
+{
+    unsigned long r,sum=0,temp;    
+    temp=n;    
+    while(n>0)    
+    {    
+    r=n%10;    
+    sum=(sum*10)+r;    
+    n=n/10;    
+    }    
+    if(temp==sum)    
+    return (1);
+    else    
+    return 0;  
+}
