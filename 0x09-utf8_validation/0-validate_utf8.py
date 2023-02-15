eb1435eb7ee6b583ad41a9f846472518e9187a3b
jaafar <jaafarferes@gmail.com>
2023-02-15 13:54:25 +0100

    utf8 validation function using Pyhon

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,31 +5,25 @@ determines if a given data set represents a valid UTF-8 encoding
 
 
 def validUTF8(data):
-    """_summary_
+    """
     Return: True if data is a valid UTF-8 encoding,
     else return False
     """
-    def countonce(num):
-        """count the number of leading 1's in a byte
-        representation of a UTF-8 encoded character.
-        """
+    i = 0
+    while i < len(data):
         count = 0
-        for i in range(7, -1, -1):
-            if num & (1 << i):
-                count += 1
-            else:
-                break
-        return count
-    count = 0
-    for a in data:
-        if not count:
-            count = countonce(a)
-            if count == 0:
-                continue
-            if count == 1 or count > 4:
-                return False
+        mask = 1 << 7
+        while mask & data[i]:
+            count += 1
+            mask >>= 1
+        if count == 0:
+            pass
+        elif count == 1 or count > 4:
+            return False
         else:
-            count -= 1
-            if countonce(a) != 1:
-                return False
-    return count == 0
+            for j in range(i + 1, i + count):
+                if j >= len(data) or (data[j] >> 6) != 2:
+                    return False
+            i += count - 1
+        i += 1
+    return True
