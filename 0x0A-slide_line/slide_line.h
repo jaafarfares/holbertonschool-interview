b592bf43744ece4270160712d29891c7d67a1757
jaafar <jaafarferes@gmail.com>
2023-02-23 17:47:51 +0100

    slide_line.h

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,12 +1,13 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef SLIDE_TLINE_H_
+#define SLIDE_TLINE_H_
 
 #include <stddef.h>
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 
 
 static void print_array(int const *array, size_t size);
 int slide_line(int *line, size_t size, int direction);
