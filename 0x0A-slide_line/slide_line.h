a30c6bc3f58143b281e45d0d593c3b6454f5ce82
jaafar <jaafarferes@gmail.com>
2023-02-23 17:37:40 +0100

    first commit

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,17 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+static void print_array(int const *array, size_t size);
+int slide_line(int *line, size_t size, int direction);
+
+
+
+
+#endif
