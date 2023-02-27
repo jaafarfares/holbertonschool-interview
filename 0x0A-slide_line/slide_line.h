c063d0ad4b86827289f07a023ba9c9b8da90a19f
jaafar <jaafarferes@gmail.com>
2023-02-27 14:39:49 +0100

    all

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,13 +1,16 @@
 #ifndef SLIDE_TLINE_H_
 #define SLIDE_TLINE_H_
 
-#include <stddef.h>
-#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define SLIDE_LEFT 1
 #define SLIDE_RIGHT 2
 
+
+void slide_left(int *line, size_t size);
+void slide_right(int *line, size_t size);
+void merge_left(int *line, size_t size);
+void merge_right(int *line, size_t size);
 int slide_line(int *line, size_t size, int direction);
 #endif
