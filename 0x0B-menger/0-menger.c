b0275702f4531ba8b0e00139405acd97fc94451d
jaafar <jaafarferes@gmail.com>
2023-03-12 23:50:32 +0100

    updates

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 #include "menger.h"
+
 /**
- * menger - ....
+ * menger - ...
  * @level: ...
- * Return: ...
-*/
+ * Return: void
+ */
 void menger(int level)
 {
-    if (level < 0)
-    return;
-
     int size = pow(3, level);
+
+    if (level < 0)
+        return;
 
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
             int x = i, y = j;
-        while (x || y)
+            while (x || y)
             {
-        if (x % 3 == 1 && y % 3 == 1)
-        {
-            break;
+                if (x % 3 == 1 && y % 3 == 1)
+                {
+                    putchar(' ');
+                    break;
+                }
+                x /= 3;
+                y /= 3;
+            }
+            if (x == 0 && y == 0)
+                putchar('#');
         }
-        x /= 3;
-        y /= 3;
-}
-
-printf("%c", x || y ? ' ' : '#');
-}
-printf("\n");
-}
+        putchar('\n');
+    }
 }
