72ed9d9249772fc06ba88cb1e233245910cc8ab6
jaafar <jaafarferes@gmail.com>
2023-03-12 23:53:57 +0100

    updates

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -9,29 +9,28 @@
  */
 void menger(int level)
 {
-    int size = pow(3, level);
+int size = pow(3, level);
 
-    if (level < 0)
-        return;
-
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            int x = i, y = j;
-            while (x || y)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                {
-                    putchar(' ');
-                    break;
-                }
-                x /= 3;
-                y /= 3;
-            }
-            if (x == 0 && y == 0)
-                putchar('#');
-        }
-        putchar('\n');
-    }
+if (level < 0)
+return;
+for (int i = 0; i < size; i++)
+{
+for (int j = 0; j < size; j++)
+{
+int x = i, y = j;
+while (x || y)
+{
+if (x % 3 == 1 && y % 3 == 1)
+{
+putchar(' ');
+break;
+}
+x /= 3;
+y /= 3;
+}
+if (x == 0 && y == 0)
+putchar('#');
+}
+putchar('\n');
+}
 }
