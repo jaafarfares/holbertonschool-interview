f8e6a32e5e6d03db29a5f1d6b97000c77a5a1e22
jaafar <jaafarferes@gmail.com>
2023-04-12 11:54:59 +0100

    test

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -7,18 +7,16 @@ const movieid =  'https://swapi-api.hbtn.io/api/films/' + args;
 req(movieid, async function(err, res, body) {
   if (err) {
     console.error(err);
-  } else {
-    const movie = JSON.parse(body);
-    const characters = movie.characters;
-    await characters.forEach(characterUrl => {
-      req(characterUrl, function(err, res, body) {
+  } 
+  for (const id of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(id, (err, res, body) => {
         if (err) {
-          console.error(err);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-          resolve();
+          reject(err);
         }
+        const character = JSON.parse(body);
+        console.log(character.name);
+        resolve();
       });
     });
   }
