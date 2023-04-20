f0e0a405c6cbed163867fe17dd840ada570eb783
jaafar <jaafarferes@gmail.com>
2023-04-20 09:33:42 +0100

    Integer indicating total amount of rainwater retained

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def rain(walls):
+    """
+    Return: Integer indicating total amount of rainwater retained.
+    """
+    if not walls:
+        return 0
+    n = len(walls)
+    left = [0] * n
+    right = [0] * n
+    left[0] = walls[0]
+    for i in range(1, n):
+        left[i] = max(left[i-1], walls[i])
+    right[n-1] = walls[n-1]
+    for i in range(n-2, -1, -1):
+        right[i] = max(right[i+1], walls[i])
+    water = 0
+    for i in range(n):
+        level = min(left[i], right[i])
+        water += max(level - walls[i], 0)
+    return water
