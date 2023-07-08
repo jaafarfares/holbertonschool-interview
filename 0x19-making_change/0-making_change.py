3d1fddf3caca9c6d614ede1608d6effc90def16c
jaafar <jaafarferes@gmail.com>
2023-07-08 10:36:01 +0100

    final fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -9,24 +9,20 @@ def makeChange(coins, total):
     determine the fewest number of coins
     needed to meet a given amount total
     """
-    if total == 0:
+    if total <= 0:
         return 0
-
-    if total < 0:
-        return -1
 
     if len(coins) == 0:
         return -1
-
-    min_coins = float('inf')
-
-    for coin in coins:
-        if coin <= total:
-            num_coins = makeChange(coins, total - coin)
-            if num_coins != -1:
-                min_coins = min(min_coins, num_coins + 1)
-
-    if min_coins == float('inf'):
+    coins = sorted(coins)
+    a = [float('inf')] * (total + 1)
+    a[0] = 0
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if a[i - coin] != -1:
+                a[i] = min(a[i - coin] + 1, a[i])
+    if a[total] == float('inf'):
         return -1
-
-    return min_coins
+    return a[total]
