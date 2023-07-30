bf36529a219d53033a3e671c87add3872a3f91af
jaafar <jaafarferes@gmail.com>
2023-07-30 12:37:24 +0100

    calculate an island parameter based on a grid list

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -2,18 +2,31 @@
 """
 calculate an island parameter based on a grid list
 """
-
 def island_perimeter(grid):
     """
     Implement island traversal
     """
+    count = 0
+    for column in range(len(grid)):
+        for row in range(len(grid[column])):
+            if grid[column][row] == 1:
+                if column - 1 < 0 or grid[column - 1][row] == 0:
+                    count += 1
+                if column + 1 >= len(grid) or grid[column + 1][row] == 0:
+                    count += 1
+                if row + 1 >= len(grid[column]) or grid[column][row + 1] == 0:
+                    count += 1
+                if row - 1 < 0 or grid[column][row - 1] == 0:
+                    count += 1
+    return count
+
+""" def island_perimeter(grid):
+
     if type(grid) is not list:
-        return "grid must be a list"
+        return ""
     def dfs(row, col):
-        """
         depth first Search DFS or breadth first Search
         to traverse the grid and identify islands
-        """
         if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == 0:
             return 1
 
@@ -42,3 +55,4 @@ def island_perimeter(grid):
     # Step 4: Sum up the perimeters to get the total island parameter
     total_island_parameter = sum(island_perimeters)
     return total_island_parameter
+ """
