edb34e6c747b3d4f32a68bb16b26b67b3c97fede
jaafar <jaafarferes@gmail.com>
2023-09-20 17:19:29 +0100

    fix

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,24 +1,17 @@
 #!/usr/bin/node
-const req = require('request');
-
-const args = process.argv[2];
-const movieid =  'https://swapi-api.hbtn.io/api/films/' + args;
-
-req(movieid, async function(err, res, body) {
-  if (err) {
-    console.error(err);
-  } 
-  for (const id of JSON.parse(body).characters) {
-    await new Promise((resolve, reject) => {
-      request(id, (err, res, body) => {
-        if (err) {
-          reject(err);
-        }
-        const character = JSON.parse(body);
-        console.log(character.name);
-        resolve();
-      });
-    });
+const request = require('request');
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    recursionRequestPrint(characters, 0);
   }
 });
 
+function recursionRequestPrint (url, index) {
+  request(url[index], (err, res, body) => {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+    }
+  });
+}
