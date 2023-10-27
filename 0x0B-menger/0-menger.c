9acffe4a9f7840280df500721594aa834b85cc11
jaafar <jaafarferes@gmail.com>
2023-10-27 09:41:45 +0100

    *

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,34 +1,53 @@
 #include "menger.h"
+#include <stdio.h>
+#include <math.h>
+
 /**
- * menger - ...
- * @level: ...
- * Return: void
+ * menger - ............
+ * @level: ............
  */
 void menger(int level)
 {
-    int sponge_size = pow(3, level);
-    int row_idx, col_idx, print_symbol;
+	if (level < 0)
+		return;
 
-    if (level < 0)
-        return;
+	int size = pow(3, level);
 
-    for (int i = 0; i < sponge_size; i++)
-    {
-        for (int j = 0; j < sponge_size; j++)
-        {
-            print_symbol = '#';
-            row_idx = i; 
-            col_idx = j; 
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			drawMenger(size, level, i, j);
+		}
+		printf("\n");
+	}
+}
 
-            while (row_idx || col_idx)
-            {
-                if (row_idx % 3 == 1 && col_idx % 3 == 1)
-                    print_symbol = ' ';
-                row_idx /= 3;
-                col_idx /= 3;
-            }
-            putchar(print_symbol);
-        }
-        putchar('\n');
-    }
+/**
+ * drawMenger - ............
+ * @size: ............
+ * @level: ............
+ * @x: ............
+ * @y: ............
+ */
+void drawMenger(int size, int level, int x, int y)
+{
+	if (level == 0)
+	{
+		printf("#");
+	}
+	else
+	{
+		int step = size / 3;
+		int center = size / 3;
+
+		if (x >= center && x < center * 2 && y >= center && y < center * 2)
+		{
+			printf(" ");
+		}
+		else
+		{
+			drawMenger(step, level - 1, x % center, y % center);
+		}
+	}
 }
